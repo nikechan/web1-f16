1ff1846532b1b9b5c85237dd0a6cf103773073a1
Andrew Ringler <ringler@gmail.com>
2016-08-24 20:41:48 -0400

    sync updates

--- push-to-server.sh
+++ push-to-server.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "$0")"
 
 # copy web1-f16 directory to massart.andrewringler.com overwriting previous
-/usr/bin/rsync --cvs-exclude --delete -avz -e ssh . massart.andrewringler.com:/home/andrewringler_massart/massart.andrewringler.com/www/web1-f16/
+/usr/bin/rsync --cvs-exclude --exclude=".git/" --exclude=".DS_Store" --delete -avz -e ssh . massart.andrewringler.com:/home/andrewringler_massart/massart.andrewringler.com/www/web1-f16/
